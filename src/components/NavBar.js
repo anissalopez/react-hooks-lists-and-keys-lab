aaf31ae8680c197ef0e0ac60b0353d28beaee9b0
anissalopez <anissa965@gmail.com>
2023-06-26 14:29:04 -0400

    learning react

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+console.log(links)
+  return (
+    <nav>{links.map((link) => {
+      return <a href={`#${link}`} key ={link}>{link}</a>
+    })}</nav>
+  ) 
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,16 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  
+  const projectObject = projects.map((project) => {
+    return (
+      <ProjectItem name = {project.name} about = {project.about} technologies = {project.technologies} key = {project.id} />
+    )
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectObject}</div>
     </div>
   );
 }
